435a60357c5f6ef74cf541e090e8fce3416d3f16
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-04-25 19:21:13 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :tweets
+  has_many  :tweets
+
   has_secure_password
 
   def slug
@@ -9,4 +10,5 @@ class User < ActiveRecord::Base
   def self.find_by_slug(slug)
     User.all.find{|user| user.slug == slug}
   end
+
 end
