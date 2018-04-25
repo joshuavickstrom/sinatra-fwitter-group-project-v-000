1cfba5cc41741769f407e21a35f43bb4853221b9
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-04-25 19:02:05 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,20 @@ class UsersController < ApplicationController
   end
 
   get '/signup' do
+    if !logged_in?
+      erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
+    else
+      redirect to '/tweets'
+    end
+  end
+
+  post '/signup' do
     if params[:username] == "" || params[:email] == "" || params[:password] == ""
       redirect to '/signup'
     else
       @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
       @user.save
-      session[:user_id] = @user.idea
+      session[:user_id] = @user.id
       redirect to '/tweets'
     end
   end
@@ -23,7 +31,7 @@ class UsersController < ApplicationController
     end
   end
 
-  post 'login' do
+  post '/login' do
     user = User.find_by(:username => params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
